6b7a444219bc0ce4ad1ab168934f6ba53d40e43a
TheLucas0828 <lucasdsama@gmail.com>
2022-11-29 15:41:23 -0500

    Xs now appear, score now has ui

--- ChrisGaveMeThisFolder - Copy/Assets/Ninja/Scripts/DeletionField.cs
+++ ChrisGaveMeThisFolder - Copy/Assets/Ninja/Scripts/DeletionField.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DeletionField : MonoBehaviour {
 
 	public float lostLives = 0f;
-	
+	public float score = 0;
+	public Text scoreText;
+	public Text hitText;
+
+
+
 	// Update is called once per frame
 	void Update () {
 		if(lostLives >= 3f)
         {
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
+
+		scoreText.text = score.ToString();
 	}
 
 	void OnCollisionEnter(Collision col)
@@ -20,6 +28,8 @@ public class DeletionField : MonoBehaviour {
 		if(col.gameObject.GetComponentInParent<Fruit>() != null)
         {
 			lostLives++;
+
+			hitText.text += "X";
         }
 
 		Destroy(col.gameObject);
--- ChrisGaveMeThisFolder - Copy/Assets/Ninja/Scripts/FruitCut.cs
+++ ChrisGaveMeThisFolder - Copy/Assets/Ninja/Scripts/FruitCut.cs
@@ -6,12 +6,20 @@ public class FruitCut : MonoBehaviour
 {
 
     public GameObject fruitSlicedPrefab;
+    public DeletionField df;
+    
 
+    void Start()
+    {
+        df = GameObject.Find("DeletionField").GetComponent<DeletionField>();
+    }
   
 
     void OnTriggerEnter(Collider col)
     {
             Instantiate(fruitSlicedPrefab, transform.position, transform.rotation);
+            df.score += 1;
+            
             Destroy(gameObject);
     }
 }
